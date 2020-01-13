992487233e229975eaf821b9b2233ec37e7dca84
shen.xudong <shen.xudong@immomo.com>
2020-01-13 21:55:27 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mkdocs-custom-plugin',
+    version='0.0.1',
+    description='Some Desc',
+    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    author='Xudong Shen',
+    author_email='some@some.com',
+    license='unlicense',
+    packages=['xudshen'],
+    zip_safe=False
+)
